7c5d6ed59ce43b37edda6a528b8f9a61839e9c1a
Kiril Gusakov <gusakov24@gmail.com>
2021-11-05 09:43:04 +0300

    ferst commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    static Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!");
+
+        log.info("Ready");
+    }
+
+
+}
